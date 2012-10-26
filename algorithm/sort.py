20bb509464e31e2500e94d8fa2f1ef1c75561047
brenden17 <chulwook.jeon@gmail.com>
2012-10-26 17:18:31 +1000

    update

---
+++ algorithm/sort.py
@@ -0,0 +1,190 @@
+'''
+@author: Brenden
+'''
+import unittest
+import threading
+    
+def locknrelease(threading):
+    def _locknrelease(func):
+        def __locknrelease(*args, **kwargs):
+            threading.acquire()
+            result = func(*args, **kwargs)
+            threading.release()
+            return result
+        return __locknrelease
+    return _locknrelease
+            
+
+lock = threading.Lock()
+
+class ThreadSafeStack(object):
+    """poor performance"""
+    def __init__(self):
+        self.l = []
+    
+    @locknrelease(lock)    
+    def is_empty(self):
+        return self.l == []
+        
+    @locknrelease(lock)
+    def push(self, item):
+        self.l.append(item)
+    
+    @locknrelease(lock)
+    def pop(self):
+        return self.l.pop() if self.l else None
+
+class ThreadSafeQueue(object):
+    """poor performance"""
+    def __init__(self):
+        self.l = []
+    
+    @locknrelease(lock)    
+    def is_empty(self):
+        return self.l == []
+        
+    @locknrelease(lock)
+    def enqueue(self, item):
+        self.l.append(item)
+    
+    @locknrelease(lock)
+    def dequeue(self):
+        return self.l.pop(0) if self.l else None
+
+def selection_sort(l):
+    n = len(l)
+    m = 0
+    for i in range(n):
+        m = i
+        for j in range(i+1, n):
+            if l[m] > l[j]:
+                m = j
+        l[i], l[m] = l[m], l[i]
+    return l
+
+def insertion_sort(l):
+    n = len(l)
+    for i in range(1, n):
+        for j in range(i, 0, -1):
+            if l[j-1] > l[j]:
+                l[j-1], l[j] = l[j], l[j-1]
+            else:
+                break
+    return l
+
+import random
+def knuth_shuffle(l):
+    n = len(l)
+    for i in range(1, n):
+        r = random.randint(0, i)
+        l[i], l[r] = l[r], l[i]
+    return l
+
+def merge(ll, rl):
+    nl = []
+    lli, llm = 0, len(ll)
+    rli, rlm = 0, len(rl)
+    while(True):
+        if llm == lli and rlm == rli:
+            break
+        if rlm == rli or lli < llm and ll[lli] <= rl[rli]:
+            nl.append(ll[lli])
+            lli = lli + 1
+        else:
+            nl.append(rl[rli])
+            rli = rli + 1
+    return nl    
+
+def merge_sort(l):
+    n = len(l)/2
+    if n < 1:
+        return l
+    ll, rl = merge_sort(l[:n]), merge_sort(l[n:])
+    return merge(ll, rl)
+
+class ConvexHull(object):
+    def __init__(self):
+        pass
+
+    def get_angle(self, p1, p2):
+        import math
+        deltax = abs(p1[0] - p2[0])
+        deltay = abs(p1[1] - p2[1])
+        
+        angle_rad = math.atan2(deltay, deltax)
+        return angle_rad * 180.0 / math.pi
+
+    def polar_order(self, p, l): 
+        nl = [(point, self.get_angle(p, point))for point in l]
+        return [p[0] for p in sorted(nl, key=lambda p: p[1])]
+    
+    def ccw(self, p1, p2, p3):
+        x, y = 0, 1
+        v = (p2[x] - p1[x]) * (p3[y] - p3[y]) - (p2[y] - p1[y]) * (p3[x] - p1[x])
+        return False if v < 0 else True
+        
+    def graham_scan(self, p, l):
+        vertexs = [p]
+        n = len(l)
+        for i in range(n):
+            if i == 0:
+                vertexs.append(l[0])
+                continue
+            if self.ccw(vertexs[-2], vertexs[-1], l[i]):
+                vertexs.append(l[i])
+            else:
+                vertexs.pop()
+                vertexs.append(l[i])
+        return vertexs
+
+class Test(unittest.TestCase):
+    def test_stack(self):
+        s = ThreadSafeStack()
+        s.push(1)
+        self.assertEquals(False, s.is_empty())
+
+    def test_queue(self):
+        q = ThreadSafeQueue()
+        q.enqueue(1)
+        q.enqueue(2)
+        self.assertEquals(False, q.is_empty())
+        self.assertEquals(1, q.dequeue())
+        self.assertEquals(2, q.dequeue())
+        self.assertEquals(None, q.dequeue())
+
+    def test_selection_sort(self):
+        l = [7, 10, 5, 3, 8, 4, 2, 9, 6]
+        r = [2, 3, 4, 5 ,6 ,7 ,8, 9, 10]
+        self.assertEquals(r, selection_sort(l))
+ 
+    def test_insertion_sort(self):
+        l = [7, 10, 5, 3, 8, 4, 2, 9, 6]
+        r = [2, 3, 4, 5 ,6 ,7 ,8, 9, 10]
+        self.assertEquals(r, insertion_sort(l)) 
+        
+    def test_knuth_shuffle(self):
+        l = [7, 10, 5, 3, 8, 4, 2, 9, 6]
+        r = [2, 3, 4, 5 ,6 ,7 ,8, 9, 10]
+#        self.assertEquals(r, knuth_shuffle(l))
+
+    def test_merge(self):
+        ll = [3, 4, 5]
+        rl = [1, 4, 6]
+        self.assertEquals([1, 3, 4, 4, 5, 6], merge(ll, rl))
+    
+    def test_merge_sort(self):
+        l = [7, 10, 5, 3, 8, 4, 2, 9, 6]
+        r = [2, 3, 4, 5 ,6 ,7 ,8, 9, 10]
+        self.assertEquals(r, merge_sort(l))
+        
+    def test_polar_order(self):
+        ch = ConvexHull()
+        l = [(1, 1), (4, 1), (2, 2), (1, 3), (4, 3), (2, 4), (5, 4)]
+        r = [(4, 1), (4, 3), (5, 4), (2, 2), (2, 4), (1, 3)]
+        v = [(1, 1), (4, 1), (5, 4), (2, 4), (1, 3)]
+        nl = ch.polar_order(l[0], l[1:])
+        self.assertEquals(r, nl)
+        self.assertEquals(v, ch.graham_scan(l[0], nl))
+        
+if __name__ == '__main__':
+    unittest.main()
--- algorithm/tree.py
+++ algorithm/tree.py
@@ -1,146 +1,8 @@
 '''
-Created on 2012. 10. 10.
-
 @author: Brenden
-
 '''
-asdkf;asjfd;
 import unittest
-import threading
     
-def locknrelease(threading):
-    def _locknrelease(func):
-        def __locknrelease(*args, **kwargs):
-            threading.acquire()
-            result = func(*args, **kwargs)
-            threading.release()
-            return result
-        return __locknrelease
-    return _locknrelease
-            
-
-lock = threading.Lock()
-
-class ThreadSafeStack(object):
-    """poor performance"""
-    def __init__(self):
-        self.l = []
-    
-    @locknrelease(lock)    
-    def is_empty(self):
-        return self.l == []
-        
-    @locknrelease(lock)
-    def push(self, item):
-        self.l.append(item)
-    
-    @locknrelease(lock)
-    def pop(self):
-        return self.l.pop() if self.l else None
-
-class ThreadSafeQueue(object):
-    """poor performance"""
-    def __init__(self):
-        self.l = []
-    
-    @locknrelease(lock)    
-    def is_empty(self):
-        return self.l == []
-        
-    @locknrelease(lock)
-    def enqueue(self, item):
-        self.l.append(item)
-    
-    @locknrelease(lock)
-    def dequeue(self):
-        return self.l.pop(0) if self.l else None
-
-def selection_sort(l):
-    n = len(l)
-    m = 0
-    for i in range(n):
-        m = i
-        for j in range(i+1, n):
-            if l[m] > l[j]:
-                m = j
-        l[i], l[m] = l[m], l[i]
-    return l
-
-def insertion_sort(l):
-    n = len(l)
-    for i in range(1, n):
-        for j in range(i, 0, -1):
-            if l[j-1] > l[j]:
-                l[j-1], l[j] = l[j], l[j-1]
-            else:
-                break
-    return l
-
-import random
-def knuth_shuffle(l):
-    n = len(l)
-    for i in range(1, n):
-        r = random.randint(0, i)
-        l[i], l[r] = l[r], l[i]
-    return l
-
-def merge(ll, rl):
-    nl = []
-    lli, llm = 0, len(ll)
-    rli, rlm = 0, len(rl)
-    while(True):
-        if llm == lli and rlm == rli:
-            break
-        if rlm == rli or lli < llm and ll[lli] <= rl[rli]:
-            nl.append(ll[lli])
-            lli = lli + 1
-        else:
-            nl.append(rl[rli])
-            rli = rli + 1
-    return nl    
-
-def merge_sort(l):
-    n = len(l)/2
-    if n < 1:
-        return l
-    ll, rl = merge_sort(l[:n]), merge_sort(l[n:])
-    return merge(ll, rl)
-
-class ConvexHull(object):
-    def __init__(self):
-        pass
-
-    def get_angle(self, p1, p2):
-        import math
-        deltax = abs(p1[0] - p2[0])
-        deltay = abs(p1[1] - p2[1])
-        
-        angle_rad = math.atan2(deltay, deltax)
-        return angle_rad * 180.0 / math.pi
-
-    def polar_order(self, p, l): 
-        nl = [(point, self.get_angle(p, point))for point in l]
-        return [p[0] for p in sorted(nl, key=lambda p: p[1])]
-    
-    def ccw(self, p1, p2, p3):
-        x, y = 0, 1
-        v = (p2[x] - p1[x]) * (p3[y] - p3[y]) - (p2[y] - p1[y]) * (p3[x] - p1[x])
-        return False if v < 0 else True
-        
-    def graham_scan(self, p, l):
-        vertexs = [p]
-        n = len(l)
-        for i in range(n):
-            if i == 0:
-                vertexs.append(l[0])
-                continue
-            if self.ccw(vertexs[-2], vertexs[-1], l[i]):
-                vertexs.append(l[i])
-            else:
-                vertexs.pop()
-                vertexs.append(l[i])
-        return vertexs
-
 class BinaryHeap(object):
     def __init__(self):
         self.l = [None]
@@ -205,17 +67,14 @@ class HeapSort(BinaryHeap):
         return self.nl
 
 from functools import total_ordering
-RED, BLACK = True, False
-
 @total_ordering
 class Node(object):
-    def __init__(self, key=None, value=None, count=1, color=RED):
+    def __init__(self, key=None, value=None, count=1):
         self.key = key
         self.value = value
         self.right = None
         self.left = None
         self.count = count
-        self.color = color
     
     def __eq__(self, other):
         if other == None:
@@ -228,34 +87,17 @@ class Node(object):
 class BinarySearchTree(object):
     def __init__(self):
         self.root = None
-        self.depth = 0
         
     def size(self, node):
         return 0 if node == None else node.count
     
-    def insert_old(self, parent, node):
-        if parent.key > node.key:
-            if parent.left:
-                self.insert_old(parent.left, node)
-            else:
-#                print 'made new left node ', node.key
-                parent.left = node
-        else:
-            if parent.right:
-                self.insert_old(parent.right, node)
-            else:
-#                print 'made new right node ', node.key
-                parent.right = node
-        parent.count = 1 + self.size(parent.left) + self.size(parent.right)
-        
-    def put_old(self, key, value):
-        node = Node(key, value)
-        if self.root:
-            self.insert_old(self.root, node) 
-            self.root.count = 1 + self.size(self.root.left) + self.size(self.root.right)
-        else:
-            self.root = node
-        
+    def depth(self, node):
+        if node == None:
+            return 0
+        l = 1 + self.depth(node.left)
+        r = 1 + self.depth(node.right)
+        return l if l > r else r
+    
     def insert(self, node, key, value):
         if node == None:
             return Node(key, value)
@@ -297,46 +139,27 @@ class BinarySearchTree(object):
     
     def min(self, node):
         if node == None:
-            return None
+            return None, 0
+        num = 0
         while node.left:
             node = node.left
-        return node
+            num += 1
+        return node, num
     
     def max(self, node):
         if node == None:
-            return None
+            return None, 0
+        num = 0
         while node.right:
             node = node.right
-        return node
-    
-    # implementation not finished 
-    def delete_old(self, key):
-        if not self.root:
-            return None
-        
-        node = self.root
-        parent = self.root
-        while True:
-            if node.key == key:
-                if parent == node:
-                    self.root = None
-                return node
-            elif node.key > key:
-                if not node.left:
-                    return None
-                node = node.left
-            else:
-                if not node.right:
-                    return None
-                node = node.right
-        return None
+            num += 1
+        return node, num
     
     def delete_node(self, node):
         if node.left == None:
             return node.right
         node.left = self.delete_node(node.left)
         node.count = 1 + self.size(node.left) + self.size(node.right)
-    
     
     def delete_minnode(self):
         self.root = self.delete_node(self.root)
@@ -355,7 +178,7 @@ class BinarySearchTree(object):
                 return node.right
             
             tnode = node
-            node = self.min(tnode)
+            node, _ = self.min(tnode)
             node.right = self.delete_node(tnode.right)
             node.left = tnode.left
             
@@ -364,13 +187,42 @@ class BinarySearchTree(object):
     
     def delete(self, key):
         self.root = self._delete(self.root, key)
-        
-    def display(self):
-        if not self.root:
-            print ''
-
-class RedBlackBST(BinarySearchTree):
     
+    def _display(self, node, graph):
+        if not node:
+            return None
+        
+        if str(node.key) not in graph:
+            graph.add_node(str(node.key), radius=10, stroke=(0, 0, 0, 0.8))
+            
+        if node.right:
+            if str(node.right.key) not in graph:
+                graph.add_node(str(node.right.key), radius=10, stroke=(0, 0, 0, 0.8))
+            graph.add_edge(graph[str(node.key)], graph[str(node.right.key)], stroke=(0, 0, 0, 0.6))
+            self._display(node.right, graph)
+        
+        if node.left:
+            if str(node.left.key) not in graph:
+                graph.add_node(str(node.left.key), radius=10, stroke=(0, 0, 0, 0.8))
+            graph.add_edge(graph[str(node.key)], graph[str(node.left.key)], stroke=(0, 0, 0, 0.6))
+            self._display(node.left, graph)
+            
+    def display(self):
+        from pattern.graph import Graph, export
+        graph = Graph()
+        self._display(self.root, graph)
+        export(graph, 'avl', directed=True, weight=0.6, distance=6)
+            
+RED, BLACK = True, False
+class RBNode(Node):
+    def __init__(self, key, value, color=RED):
+        Node.__init__(self, key, value)
+        self.color = color
+        
+class RedBlackBST(BinarySearchTree):
+    def __init__(self):
+        BinarySearchTree.__init__(self)
+        
     def is_red(self, node):
         return node.color == RED if node else False
     
@@ -394,7 +246,6 @@ class RedBlackBST(BinarySearchTree):
         return x
     
     def flip_color(self, h):
-#        if self.is_red(h) or \
         if not self.is_red(h.left) or \
             not self.is_red(h.right):
             return None
@@ -406,14 +257,14 @@ class RedBlackBST(BinarySearchTree):
 #        print '-' * 20
 #        print 'key ', key
         if node == None:
-            return Node(key, value)
+            return RBNode(key, value)
         if node.key > key:
             node.left = self.insert(node.left, key, value)
         else:
             node.right = self.insert(node.right, key, value)
             
         node.count = 1 + self.size(node.left) + self.size(node.right)
-        
+
         if self.is_red(node.right) and not self.is_red(node.left):
 #            print 'rotate_left'
             node = self.rotate_left(node)
@@ -424,56 +275,69 @@ class RedBlackBST(BinarySearchTree):
 #            print 'flip_color'
             self.flip_color(node)
         return node
+
+class AVLTree(BinarySearchTree):
+    def __init__(self):
+        BinarySearchTree.__init__(self)
+    
+    def height_size(self, node):
+        return 0 if not node else node.height
+    
+    def rotate_right(self, node):
+        x = node.left
+        node.left = x.right
+        x.right = node
+        node.hegiht = self.depth(node)
+        return x
+    
+    def rotate_left(self, node):
+        x = node.right
+        node.right = x.left
+        x.left = node
+        node.hegiht = self.depth(node)
+        return x
+
+    def insert(self, node, key, value):
+#        print '-' * 20
+#        print 'key ', key
+        if node == None:
+            return Node(key, value)
+        if node.key > key:
+            node.left = self.insert(node.left, key, value)
+        else:
+            node.right = self.insert(node.right, key, value)
+            
+        # rebuild
+        b = self.depth(node.left) - self.depth(node.right)
+        if b == 2:
+            leftsize = 0 if not node.left.left else self.depth(node.left.left)
+            rightsize = 0 if not node.left.right else self.depth(node.left.right)
+            if leftsize - rightsize == 1:
+#                print 'rotate_right'
+                node = self.rotate_right(node)
+            if leftsize - rightsize == -1:
+#                print 'rotate_left'
+#                print 'rotate_right'
+                node.left = self.rotate_left(node.left)
+                node = self.rotate_right(node)
+        
+        if b == -2:
+            leftsize = 0 if not node.right.left else self.depth(node.right.left)
+            rightsize = 0 if not node.right.right else self.depth(node.right.right)
+            if leftsize - rightsize == -1:
+#                print 'rotate_left'
+                node = self.rotate_left(node)
+            if leftsize - rightsize == 1:
+#                print 'rotate_right'
+#                print 'rotate_left'
+                node.right = self.rotate_right(node.right)
+                node = self.rotate_left(node)
+
+        node.count = 1 + self.size(node.left) + self.size(node.right)
+        node.height = self.depth(node)
+        return node
     
 class Test(unittest.TestCase):
-    def test_stack(self):
-        s = ThreadSafeStack()
-        s.push(1)
-        self.assertEquals(False, s.is_empty())
-
-    def test_queue(self):
-        q = ThreadSafeQueue()
-        q.enqueue(1)
-        q.enqueue(2)
-        self.assertEquals(False, q.is_empty())
-        self.assertEquals(1, q.dequeue())
-        self.assertEquals(2, q.dequeue())
-        self.assertEquals(None, q.dequeue())
-
-    def test_selection_sort(self):
-        l = [7, 10, 5, 3, 8, 4, 2, 9, 6]
-        r = [2, 3, 4, 5 ,6 ,7 ,8, 9, 10]
-        self.assertEquals(r, selection_sort(l))
- 
-    def test_insertion_sort(self):
-        l = [7, 10, 5, 3, 8, 4, 2, 9, 6]
-        r = [2, 3, 4, 5 ,6 ,7 ,8, 9, 10]
-        self.assertEquals(r, insertion_sort(l)) 
-        
-    def test_knuth_shuffle(self):
-        l = [7, 10, 5, 3, 8, 4, 2, 9, 6]
-        r = [2, 3, 4, 5 ,6 ,7 ,8, 9, 10]
-#        self.assertEquals(r, knuth_shuffle(l))
-
-    def test_merge(self):
-        ll = [3, 4, 5]
-        rl = [1, 4, 6]
-        self.assertEquals([1, 3, 4, 4, 5, 6], merge(ll, rl))
-    
-    def test_merge_sort(self):
-        l = [7, 10, 5, 3, 8, 4, 2, 9, 6]
-        r = [2, 3, 4, 5 ,6 ,7 ,8, 9, 10]
-        self.assertEquals(r, merge_sort(l))
-        
-    def test_polar_order(self):
-        ch = ConvexHull()
-        l = [(1, 1), (4, 1), (2, 2), (1, 3), (4, 3), (2, 4), (5, 4)]
-        r = [(4, 1), (4, 3), (5, 4), (2, 2), (2, 4), (1, 3)]
-        v = [(1, 1), (4, 1), (5, 4), (2, 4), (1, 3)]
-        nl = ch.polar_order(l[0], l[1:])
-        self.assertEquals(r, nl)
-        self.assertEquals(v, ch.graham_scan(l[0], nl))
-        
     def test_binary_heap(self):
         bh = BinaryHeap()
         bh.insert(2)
@@ -498,6 +362,12 @@ class Test(unittest.TestCase):
         
     def test_binary_search_tree(self):
         l = (3, 'A'), (2, 'B'), (1, 'C'), (4, 'D'), (10, 'E'), (17, 'F')
+        """
+             3
+           2   4
+         1       10
+                   17
+        """
         bst = BinarySearchTree()
         for e in l:
             key, value = e
@@ -505,11 +375,13 @@ class Test(unittest.TestCase):
         self.assertEquals('C', bst.get(1))
         self.assertEquals('A', bst.get(3))
         self.assertEquals(6, bst.root.count)
-        self.assertEqual(1, bst.min(bst.root).key)
+        self.assertEqual(1, bst.min(bst.root)[0].key)
+        self.assertEqual(2, bst.min(bst.root)[1])
+        self.assertEqual(3, bst.max(bst.root)[1])
+        self.assertEqual(4, bst.depth(bst.root))
         bst.delete(3)
-        self.assertEquals(5, bst.root.count)        
-
-               
+        self.assertEquals(5, bst.root.count)
+        
     def test_LLRB(self):
         from string import ascii_uppercase
         from random import shuffle, seed
@@ -523,6 +395,15 @@ class Test(unittest.TestCase):
             key, value = e
             rbt.put(key, value)
         self.assertEquals(size, rbt.root.count)
-            
+    
+    def test_AVL(self):
+        avl = AVLTree()
+        z = [(3, 'H'), (2, 'Y'), (1, 'F'), (4, 'M'), (5, 'J'), (6, 'E'), (7, 'K'), (16, 'C'), (15, 'P') ]
+        for e in z:
+            key, value = e
+            avl.put(key, value)
+#        avl.display()  
+
+        
 if __name__ == '__main__':
     unittest.main()
