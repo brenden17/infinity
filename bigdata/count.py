e7f9519ec5a9840e010d533c5aba8b225e738daf
brenden17 <chulwook.jeon@gmail.com>
2012-12-17 14:25:29 +1000

    update

---
+++ bigdata/count.py
@@ -0,0 +1,14 @@
+from mrjob.job import MRJob
+import logging
+
+class MRWordCounter(MRJob):
+	def mapper(self, key, line):
+		self.increment_counter('group', 'counter_name', 1)
+		for word in line.split():
+			yield word, 1
+
+	def reducer(self, word, occurrences):
+		yield word, sum(occurrences)
+
+if __name__ == '__main__':
+	MRWordCounter.run()
