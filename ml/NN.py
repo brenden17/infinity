4dfab93bda2df6157de5e8db993320ce6d950aa9
brenden17 <chulwook.jeon@gmail.com>
2013-04-28 21:42:49 +0900

    update

--- ml/NN.py
+++ ml/NN.py
@@ -1,21 +1,24 @@
-"""
-"""
-
 import unittest
 import numpy as np
 
+def shape(a):
+    #return a.shape if a.ndim>1 else a.shape[0], 1
+    if a.ndim>1:
+        return a.shape
+    else:
+        return a.shape[0], 1
+
 class Perception(object):
     """Perception"""
-    def __init__(self, data, target, n=4, theta=0.35):
-        m, n = data.shape
-        self.weight = np.random.rand(n+1)[:,np.newaxis]
-        self.data = np.hstack((data, -np.ones((m, 1))))
-        self.target = target[:, np.newaxis]
-        self.n = n
+    def __init__(self, data, target, itercount=0, theta=0.25):
+        dm, dn = shape(data)
+        tm, tn = shape(target)
+        assert dm==tm
+        self.weight = np.random.rand(dn+1, tn)
+        self.data = np.hstack((data, -np.ones((dm, 1))))
+        self.target = target
+        self.n = itercount if itercount > 0 else dm
         self.theta = theta
-
-    def set_option(self):
-        pass
 
     def fwd(self):
         r = np.dot(self.data, self.weight)
@@ -23,12 +26,13 @@ class Perception(object):
 
     def train(self):
         for i in range(self.n):
-            self.weight = self.weight - self.theta * np.dot(np.transpose(self.data), self.fwd())
-            #self.weight = self.weight - self.theta * np.dot(np.transpose(self.data), self.fwd()) * 0.1 - 0.05
+            self.weight = self.weight + self.theta * np.dot(np.transpose(self.data), self.target - self.fwd())
+            print self.weight
 
-    def test(self, test_data):
+    def predict(self, test_data):
         data = np.hstack((test_data, -np.ones(1)))
-        return 1 if np.dot(data, self.weight)>0.5 else 0
+        output = np.dot(data, self.weight)
+        return np.where(output>0.5, 1, 0)
 
 def sigmod(output, beta=1):
     return 1.0 / (1.0 + np.exp(output))
@@ -48,9 +52,6 @@ class MLP(object):
         self.weight1 = np.random.rand(data_n+1, hidden_layer)
         self.weight2 = np.random.rand(hidden_layer+1, target_n)
 
-    def set_option(self):
-        pass
-    
     def set_extra(self, m):
         return -np.ones((1)) if m.ndim==1 else -np.ones((m.shape[1], 1))
 
@@ -90,27 +91,43 @@ class MLP(object):
             o = -np.ones((1))
         else:
             o = -np.ones((test_input.shape[1], 1))
-            
         data = np.hstack((test_input, o))
         print data
         print '**************'
         return self.fwd(data)
 
 class Test(unittest.TestCase):
-    def perception(self):
+    def test_perception1(self):
+        print '-----test_perception1------'
         data = np.array([[0, 0], [1, 0], [0, 1], [1, 1]])
-        target = np.array([0, 1, 1, 1])
-        pcn = Perception(data, target, n=4)
+        target = np.array([[0], [1], [1], [1]])
+        pcn = Perception(data, target)
         pcn.train()
-        self.assertEquals(1, pcn.test(np.array([-1, -1])))
+        self.assertEquals(0, pcn.predict(np.array([-1, -1])))
 
-    def test_mlp(self):
+    def test_perception2(self):
+        print '-----test_perception2------'
+        data = np.array([[0, 0], [1, 0], [0, 1], [1, 1]])
+        target = np.array([[0, 0], [1, 0], [0, 1], [1, 1]])
+        pcn = Perception(data, target)
+        pcn.train()
+        self.assertEquals(np.array([1, 1]), pcn.predict(np.array([2,2])))
+
+    def perception3(self):
+        print '-----test_perception3------'
+        data = np.array([[0, 0], [1, 0], [0, 1], [1, 1]])
+        target = np.array([[1], [2], [3], [4]])
+        pcn = Perception(data, target)
+        pcn.train()
+        self.assertEquals(1, pcn.predict(np.array([-1, -1])))
+
+    def mlp(self):
         data = np.array([[0, 0], [1, 0], [0, 1], [1, 1]])
         target = np.array([1, 0, 0, 1])
         mlp = MLP(data, target) 
         mlp.train()
         print '======================'
-        self.assertEquals(1, mlp.test(np.array([-1, -1])))
+        self.assertEquals([0, 0], mlp.test(np.array([-1, -1])))
 
 if __name__ == '__main__':
     unittest.main()
