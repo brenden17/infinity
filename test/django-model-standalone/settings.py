8d66948236124016d337a76d0260ea4a5c4e0161
brenden17 <chulwook.jeon@gmail.com>
2013-07-11 15:35:11 +0900

    upate-readme.md

--- test/django-model-standalone/settings.py
+++ test/django-model-standalone/settings.py
@@ -1,7 +1,7 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'sqlite.db',
+        'NAME': '.sqlite.db',
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
