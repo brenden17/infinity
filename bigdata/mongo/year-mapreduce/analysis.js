4c727f63dd6befadc69658a7ca66c6faa5912633
Ubuntu <ubuntu@ip-172-31-36-169.us-west-2.compute.internal>
2013-07-16 02:57:58 +0000

    -update

---
+++ bigdata/mongo/year-mapreduce/analysis.js
@@ -0,0 +1,41 @@
+var setup = function(){
+    var conn = new Mongo();
+    var db = conn.getDB("yearlybalancedb");
+    return db;
+};
+
+var yearMap = function(){
+    var year = this.dayat.getFullYear().toString();
+    var month = this.dayat.getMonth() + 1;
+    var k = year.toString() + '-' +  month.toString();
+    //var k = new Array(year, month);// error 
+    var v = this.expense;
+    emit(k, {expense:v});
+};
+
+var yearReduce = function(key, values){
+    var result = {sumMonth:0};
+    values.forEach(function(value){
+        result.sumMonth += value.expense;
+    });
+    result.meanMonth = result.sumMonth / 12;
+    return result;
+};
+
+var db = setup();
+var year1112 = db.daily_balance.mapReduce(yearMap, yearReduce, {out:'year'});
+
+var compareYearMap = function(){
+    var k = '2011-2012-' + this._id.split('-')[1];
+    var v = this.value['sumMonth'];
+    emit(k, {month:v});
+};
+
+var compareYearReduce = function(key, values){
+    var result = {diff:0, percent:0};
+    result.diff = values[0].month - values[1].month;
+    result.percent = (values[0].month - values[1].month)/values[0].month*100;
+    return result;
+};
+
+var compare = db.year.mapReduce(compareYearMap, compareYearReduce, {out:'compare'});
---
+++ bigdata/mongo/year-mapreduce/generate_data.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+from random import randint
+try:
+    import mongoengine as me
+except:
+    print '== error importing mongoengine ='
+    import sys
+    sys.exit()
+
+
+class DailyBalance(me.Document):
+    dayat = me.DateTimeField(default=datetime.now)
+    income = me.FloatField()
+    expense = me.FloatField()
+
+def setup_db():
+    DBNAME = 'yearlybalancedb'
+    me.connect(DBNAME)
+
+def create_doc(date, income, expense):
+    doc = DailyBalance(date, income, expense)
+    doc.save()
+    return doc
+
+def generate_data():
+    sdate = datetime(2011, 01, 01)
+    edate = datetime(2013, 06, 30)
+    day = timedelta(days=1)
+    cdate = sdate
+    valuerange = (10000, 100000)
+    fr = randint(*valuerange)
+    while cdate < edate:
+        income = randint(*valuerange)
+        expense = randint(*valuerange)
+        create_doc(cdate, income, expense)
+        cdate += day
+
+if __name__ == '__main__':
+    setup_db()
+    generate_data()
+    print 'Done'
