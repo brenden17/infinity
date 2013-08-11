58639cff1fb1b401ea0a5fd3712bb2df4d9426e7
Ubuntu <ubuntu@ip-172-31-36-169.us-west-2.compute.internal>
2013-08-11 16:47:24 +0000

    update-mongo

--- test/mongoengine/common.py
+++ test/mongoengine/common.py
@@ -1,17 +1,77 @@
-from pymongo import Connection
+from pymongo import Connection, database
 import pandas as pd
 
-class MetaMongoBase(type):
-    pass
-
-
-class Test(object):
+class MongoHelper(object):
     def __init__(self):
         pass
 
-    def get_dataframe(self):
+    @staticmethod
+    def get_dbs():
         conn = Connection()
-        db = conn.yearlybalancedb
-        year = db.year
-        return DataFrame(list(year.find()))
+        return conn.database_names()
 
+    @staticmethod
+    def drop_db(dbname):
+        conn = Connection()
+        return conn.drop_database(dbname)
+
+    @staticmethod
+    def get_collections(dbname):
+        try:
+            conn = Connection()
+            db = database.Database(conn, dbname)
+            return db.collection_names()
+        except Exception as e:
+            print(str(e))
+
+    @staticmethod
+    def drop_collections(dbname, collection_name):
+        try:
+            conn = Connection()
+            db = database.Database(conn, dbname)
+            return db.drop_collection(collection_name)
+        except Exception as e:
+            print(str(e))
+
+    @staticmethod
+    def get_collection(dbname, collection_name):
+       try:
+           conn = Connection()
+           db = database.Database(conn, dbname)
+           return db[collection_name]
+       except Exception as e:
+           print(str(e))
+
+    @staticmethod
+    def get_dataframe(dbname, collection_name, option={}):
+       try:
+           c = MongoHelper.get_collection(dbname, collection_name)
+           return pd.DataFrame(list(c.find(**option)))
+       except Exception as e:
+           print(str(e))
+
+class MetaMongoBase(type):
+    def __new__(meta, classname, supers, classdict):
+        if 'mongometa'in classdict:
+            mongometa = classdict['mongometa']
+            dbname = mongometa.get('dbname', None)
+            collection = classname.lower()
+            classdict['get_dataframe'] = lambda self,o:MongoHelper.get_dataframe(dbname, collection, o)
+        return type.__new__(meta, classname, supers, classdict)
+
+class MongoBase(object):
+    __metaclass__ = MetaMongoBase
+    def __init__(self):
+        pass
+
+class Year(MongoBase):
+    mongometa = {'dbname':'yearlybalancedb'}
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    y = Year()
+    print(y.get_dataframe({}))
+
+    df = MongoHelper.get_dataframe('yearlybalancedb','daily_balance',{'field':['dayat']})
+    print(df.values)
