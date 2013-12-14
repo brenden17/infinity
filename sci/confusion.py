70f23d2c70950a0b962c05b109bb4cf97eee3deb
brenden17 <chulwook.jeon@gmail.com>
2013-12-14 16:30:58 +0900

    changed indent

--- sci/confusion.py
+++ sci/confusion.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 def cal_confusion(m):
-   return (float(np.trace(m))/float(np.sum(m))) * 100.0
+    return (float(np.trace(m)) / float(np.sum(m))) * 100.0
 
 if __name__ == '__main__':
-    m = np.array([[5, 3, 1],[2, 3, 1],[0, 2, 1]])
+    m = np.array([[5, 3, 1], [2, 3, 1], [0, 2, 1]])
     print cal_confusion(m)
